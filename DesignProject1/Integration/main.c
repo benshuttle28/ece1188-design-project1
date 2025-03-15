0544add258ba4c1836fd6318bc5c07fef37e3061
joelnberns <joelnberns@outlook.com>
2025-03-15 13:37:23 -0400

    Finished FSM code

--- DesignProject1/Integration/main.c
+++ DesignProject1/Integration/main.c
@@ -2,22 +2,58 @@
 #include <stdint.h>
 #include "..\inc\Clock.h"
 #include "..\inc\Reflectance.h"
-#include "..\inc\MotorSimple.h"
+#include "..\inc\TExaS.h"
+#include "..\inc\SysTickInts.h"
+#include "..\inc\CortexM.h"
 #include "..\inc\MotorFSM.h"
+
+
+uint8_t out;
+uint8_t reflectance_input;
+state_t *motor_pt;
+uint8_t reflectance_lock = 0;
+uint8_t interrupts = 0;
+uint8_t sensor_out;
+
+
+void SysTick_Handler(void){ // every 1ms
+  if ((interrupts % 10) == 0) {
+      reflectance_lock = 1;
+      Reflectance_Start();
+  }
+  else if ((interrupts % 10) == 1) {
+      sensor_out = Reflectance_End();
+      reflectance_lock = 0;
+      reflectance_input = Reflectance_Center_int(sensor_out);
+      motor_pt = motor_pt->next[reflectance_input];
+      // drive motors at given duty cycles
+      out = motor_pt->out;
+      //Illuminate_LEDs(sensor_out);
+
+
+  }
+  interrupts += 1;
+}
 
 void main() {
 
     Clock_Init48MHz();
-    Motor_InitSimple();
+    //Motor_InitSimple();
+    Reflectance_Init();
+    SysTick_Init(48000, 1);
+    EnableInterrupts();
+    TExaS_Init(LOGICANALYZER_P7);
 
-    state_t *motor_pt;
-    uint8_t reflectance_input;
     motor_pt = center;
+
+
+
     while(1) {
-        // drive motors at given duty cycles
-        reflectance_input = Reflectance_Center(1000);
-        motor_pt = motor_pt->next[reflectance_input];
+
+
+
 
     }
 
 }
+
--- DesignProject1/inc/MotorFSM.h
+++ DesignProject1/inc/MotorFSM.h
@@ -34,7 +34,7 @@ state_t FSM[5] = {
                    {0x01, hardTurnDuty, 0x00, {left, leftCenter, right, left}},
                     {0x02, centerDuty, centerDuty, {left, left, right, leftCenter}},
                      {0x03, 0x00, hardTurnDuty, {right, left, rightCenter, right}},
-                      {0x04, 0x00, centerDuty, centerDuty, {right, left, right, rightCenter}}
+                      {0x04, centerDuty, centerDuty, {right, left, right, rightCenter}}
 
 };
 
