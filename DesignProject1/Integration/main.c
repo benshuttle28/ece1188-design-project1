c80275ed717bcb0aaa9f52c79c3f23b2220d7625
joelnberns <joelnberns@outlook.com>
2025-03-19 11:40:29 -0400

    added LEDs

--- DesignProject1/Integration/main.c
+++ DesignProject1/Integration/main.c
@@ -16,6 +16,34 @@ uint8_t reflectance_lock = 0;
 uint8_t interrupts = 0;
 uint8_t sensor_out;
 
+void Illuminate_LEDs(uint8_t sensor_out) {
+    P2->OUT &= ~0x07;
+    if (sensor_out == 0x00) {
+        P2->OUT |= 0x01; // red, straight
+    }
+    else if (sensor_out == 0x01) {
+        P2->OUT |= 0x04; // blue, far left turn
+    }
+    else if (sensor_out == 0x02) {
+        P2->OUT |= 0x02; // green, left turn
+    }
+    else if (sensor_out == 0x03) {
+        P2->OUT |= 0x02; // green, left turn
+    }
+    else if (sensor_out == 0x04) {
+        P2->OUT |= 0x03; // yellow, right turn
+    }
+    else if (sensor_out == 0x05) {
+        P2->OUT |= 0x03; // yellow, right turn
+    }
+    else if (sensor_out == 0x06) {
+        P2->OUT |= 0x07; // white, hard right turn
+    }
+    else {
+        P2->OUT |= 0x05; // pink, other
+    }
+}
+
 
 void SysTick_Handler(void){ // every 1ms
   if ((interrupts % 10) == 0) {
@@ -29,7 +57,7 @@ void SysTick_Handler(void){ // every 1ms
       motor_pt = motor_pt->next[reflectance_input];
       // drive motors at given duty cycles
       out = motor_pt->out;
-      //Illuminate_LEDs(sensor_out);
+      Illuminate_LEDs(out);
 
 
   }
@@ -43,7 +71,6 @@ void main() {
     Reflectance_Init();
     SysTick_Init(48000, 1);
     EnableInterrupts();
-    TExaS_Init(LOGICANALYZER_P7);
 
     motor_pt = center;
 
